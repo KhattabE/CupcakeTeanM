09e6ea85d0231230acc4d2b22daea2ccdec409e1
Musa <Musasayed909@gmail.com>
2026-04-12 15:23:02 +0200

    small changes and setup of controller

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,8 +2,10 @@ package app;
 
 import app.config.SessionConfig;
 import app.config.ThymeleafConfig;
-import io.javalin.Javalin;
+import app.controllers.MainController;
+import app.controllers.UserController;
 import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
 public class Main {
@@ -13,17 +15,29 @@ public class Main {
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
     private static final String DB = "cupcake";
 
-    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+    private static final ConnectionPool connectionPool =
+            ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
     public static void main(String[] args) {
-        // Initializing Javalin and Jetty webserver
-        Javalin javApp = Javalin.create(config -> {
+
+        Javalin app = Javalin.create(config -> {
             config.staticFiles.add("/public");
-            config.jetty.modifyServletContextHandler(handler -> handler.setSessionHandler(SessionConfig.sessionConfig()));
+            config.jetty.modifyServletContextHandler(
+                    handler -> handler.setSessionHandler(SessionConfig.sessionConfig())
+            );
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
-            config.staticFiles.add("/templates");
+
+            config.routes.get("/", MainController::index);
+
+            config.routes.get("/signup", UserController::signUp);
+            config.routes.post("/signup", UserController::handleSignUp);
+            config.routes.get("/signin", UserController::signIn);
+
+            config.routes.get("/view-all-users", MainController::viewAllUsers);
+            config.routes.get("/build-cupcake", MainController::buildYourCupcake);
+            config.routes.get("/basket", MainController::yourBasket);
+            config.routes.get("/orders", MainController::yourOrders);
+            config.routes.get("/admin-profile", MainController::adminProfile);
         }).start(7070);
-
-
     }
 }
--- src/main/java/app/controllers/MainController.java
+++ src/main/java/app/controllers/MainController.java
@@ -1,4 +1,31 @@
 package app.controllers;
 
+
+import io.javalin.http.Context;
+
 public class MainController {
+
+    public static void index(Context ctx) {
+        ctx.render("index.html");
+    }
+
+    public static void viewAllUsers(Context ctx) {
+        ctx.render("viewAllUsers.html");
+    }
+
+    public static void buildYourCupcake(Context ctx) {
+        ctx.render("buildYourCupcake.html");
+    }
+
+    public static void yourBasket(Context ctx) {
+        ctx.render("YourBasket.html");
+    }
+
+    public static void yourOrders(Context ctx) {
+        ctx.render("YourOrders.html");
+    }
+
+    public static void adminProfile(Context ctx) {
+        ctx.render("AdminUIProfile.html");
+    }
 }
